51a26adfb475561f241e5864d7ef5c10a47991a4
kefline <maduhukefline64@gmail.com>
2024-10-01 01:51:26 +0300

    initial commit

---
+++ resources/views/instructor/instructor_dlabnav.blade.php
@@ -0,0 +1,83 @@
+<div class="dlabnav">
+    <div class="dlabnav-scroll">
+        <ul class="metismenu" id="menu">
+            <li><a href="/instructor_dashboard" class="" aria-expanded="false">
+                    <i class="bi bi-grid"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a></li>
+
+            <li><a href="/instructor_course" class="" aria-expanded="false">
+                    <i class="bi bi-book"></i>
+                    <span class="nav-text">My Courses</span>
+                </a></li>
+
+            <li><a href="/instructor_student" class="" aria-expanded="false">
+                    <i class="bi bi-people"></i>
+                    <span class="nav-text">Students</span>
+                </a></li>
+
+            <li><a href="/instructor_assignments" class="" aria-expanded="false">
+                    <i class="bi bi-pen"></i>
+                    <span class="nav-text">Assignments</span>
+                </a></li>
+
+            <li><a href="/instructor_grades" class="" aria-expanded="false">
+                    <i class="bi bi-pencil-square"></i>
+                    <span class="nav-text">Grades</span>
+                </a></li>
+
+            <li><a href="/instructor_schedule" class="" aria-expanded="false">
+                    <i class="bi bi-calendar"></i>
+                    <span class="nav-text">Schedule</span>
+                </a></li>
+
+            <li><a href="/resources" class="" aria-expanded="false">
+                    <i class="bi bi-folder"></i>
+                    <span class="nav-text">Resources</span>
+                </a>
+                
+            </li>
+
+            <li><a href="/notifications" class="" aria-expanded="false">
+                    <i class="bi bi-bell"></i>
+                    <span class="nav-text">Notifications</span>
+                </a></li>
+
+            <li><a href="/analytics" class="" aria-expanded="false">
+                    <i class="bi bi-graph-up"></i>
+                    <span class="nav-text">Analytics</span>
+                </a></li>
+
+            
+
+            <li><a href="/help" class="" aria-expanded="false">
+                    <i class="bi bi-question-circle"></i>
+                    <span class="nav-text">Help</span>
+                </a></li>
+
+            
+
+            <li><a href="/profile" class="" aria-expanded="false">
+                    <i class="bi bi-person"></i>
+                    <span class="nav-text">Profile</span>
+                </a></li>
+
+
+
+            <li><a href="/logout" class="" aria-expanded="false">
+                    <i class="bi bi-power"></i>
+                    <span class="nav-text">Logout</span>
+                </a></li>
+        </ul>
+
+        <div class="plus-box">
+            <div class="d-flex align-items-center">
+                <h5>Upgrade your Account</h5>
+                <img src="images/medal.png" alt="">
+            </div>
+            <p>Upgrade</p>
+            <a href="javascript:void(0);" class="btn btn-primary btn-sm">Upgrade</a>
+        </div>
+    </div>
+
+</div>
---
+++ resources/views/instructor/nav-header.blade.php
@@ -0,0 +1,14 @@
+<div class="nav-header">
+    <a href="index.html" class="brand-logo">
+
+
+
+
+
+    </a>
+    <div class="nav-control">
+        <div class="hamburger">
+            <span class="line"></span><span class="line"></span><span class="line"></span>
+        </div>
+    </div>
+</div>
